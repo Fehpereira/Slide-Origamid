ca7246eb81cf9b9c69e1f663b82578654cc57e27
FehPereira <fehpereira237@gmail.com>
2025-05-05 10:38:22 -0300

    custom-controls styles

--- js/script.js
+++ js/script.js
@@ -3,4 +3,4 @@ import { SlideNav } from './slide.js';
 const slideNav = new SlideNav('.slide', '.wrapper');
 slideNav.init();
 slideNav.addArrow('.prev', '.next');
-slideNav.addControl();
+slideNav.addControl('.custom-controls');
